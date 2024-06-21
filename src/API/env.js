3716e015f49e14489069ec0f5077bf36f9eae570
Eric Kha <erkha102@gmail.com>
2024-06-21 05:02:23 -0700

    Remove the env.js file

--- src/API/env.js
+++
@@ -1,9 +0,0 @@
-// Local connection to backend:
-const BASE_URL = "http://localhost:8080";
-
-// server connection to backend:
-// const BASE_URL = "http://khahost.ddns.net:8080";
-
-
-
-export default BASE_URL;
