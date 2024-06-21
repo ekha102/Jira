60fd06ac9e4fdda1cf16db43bcbc30a9588b63fd
Eric Kha <erkha102@gmail.com>
2024-06-21 05:00:33 -0700

    Add the gitIgnore for env

--- src/API/fetcher.js
+++ src/API/fetcher.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import BASE_URL from "./env";
+
 
 const fetcher = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
       'Content-Type': 'application/json',
       'ngrok-skip-browser-warning': 'true'
